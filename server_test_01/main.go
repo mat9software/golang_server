132b86f8d0500e0c9cc026a2b65a54cf49571403
Mat9Software <mat9software@gmail.com>
2023-11-04 12:09:47 -0400

    Base websever and docker development environment

--- server_test_01/main.go
+++ server_test_01/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-"fmt"
-"log"
-"net/http"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
 )
 
 func main() {
 
-http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-http.ServeFile(w, r, r.URL.Path[1:])
-})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, r.URL.Path[1:])
+	})
 
-http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-fmt.Fprintf(w, "Hi")
-})
+	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hi")
+	})
+	httpPort := os.Getenv("PORT")
+	if httpPort == "" {
+		httpPort = "80"
+	}
 
-log.Fatal(http.ListenAndServe(":8081", nil))
+	log.Print("Listening on port " + httpPort)
+	log.Fatal(http.ListenAndServe(":"+httpPort, nil))
 
 }
