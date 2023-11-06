e2503ad0a7110dc37d27a382eb06e206ad5330a6
Mat9Software <mat9software@gmail.com>
2023-11-06 12:25:31 -0500

    Added Nginx configuration. Split frontend & backend.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/frontend" element={<App />}>
+        <Route path="/static" element={<App />}>
         </Route>
       </Routes>
     </BrowserRouter>
