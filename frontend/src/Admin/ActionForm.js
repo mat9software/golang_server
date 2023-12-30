72045af0ebda7b6d60d521a84e3f8293cb457f56
Mat9Software <mat9software@gmail.com>
2023-12-29 19:40:06 -0500

    Update Action Form

--- frontend/src/Admin/ActionForm.js
+++ frontend/src/Admin/ActionForm.js
@@ -6,17 +6,43 @@ async function handleAction(e) {
     const form = e.target;
     const formData = new FormData(form);
 
-    //mdtmp fetch('/some-api', { method: form.method, body: formData });
-
     const formJson = Object.fromEntries(formData.entries());
-    console.log(formJson);
+
+    try {
+        let fetchData = {
+          method: formJson.method,
+          headers: {
+            "Content-Type": "application/json",
+          }
+        }
+        if(formJson.method === "POST")
+          fetchData.body = JSON.stringify(formJson.body)
+
+        const response = await fetch(formJson.url, fetchData)
+
+        console.log("Call ", formJson.method, " ", formJson.url, " status:", response.status)
+
+/*
+        if(response.status !== 200) {
+          console.error("Call failed", response.status)
+        }
+*/
+        //const ret = await response.json()
+        //console.log(ret)
+    } catch (e) {
+        console.error(e)
+    }
+
 }
 
 function ActionForm() {
 return (
  <>
     <form  onSubmit={handleAction}>
-      <div> Check box, POST or GET</div>
+      {/* Caution, no on change here, re-render might overwrite values.  */}
+      <label><input checked type="radio" name="method" value="POST"/>POST</label>
+      <label><input type="radio" name="method" value="GET"/>GET</label>
+      <hr />
       <label>
         URL <input name="url" />
       </label>
